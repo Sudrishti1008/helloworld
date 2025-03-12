350d7175620582cf71187a7f3e4f4b532d015f76
Sudrishti1008 <sudrishtichettri27@gmail.com>
2025-03-12 22:31:55 +0000

    addition

--- game-tic-tac-toe/src/components/ContactForm.js
+++ game-tic-tac-toe/src/components/ContactForm.js
@@ -10,6 +10,19 @@ function ContactForm() {
     message: "",
   });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted", formData);
+    alert("Form Submitted Successfully!");
+  };
 
   return (
     <div className="contact-form-container">
